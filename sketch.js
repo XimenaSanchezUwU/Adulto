19576cd376e16ca4c5868e77f8a1d815c0eaefe2
XimenaSanchezUwU <126804482+XimenaSanchezUwU@users.noreply.github.com>
2023-06-12 09:20:41 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -17,27 +17,27 @@ function draw() {
   frameRate(18);
   textSize(45);
   fill(3, 1, 179);
-  text('¿Cómo se supone que se comporta un adulto?', 340, 510);
-  text('¿Cómo se supone que se comporta un adulto?', 350, 520);
-  text('¿Cómo se supone que se comporta un adulto?', 310, 530);
-  text('¿Cómo se supone que se comporta un adulto?', 290, 540);
-  text('¿Cómo se supone que se comporta un adulto?', 270, 550);
-  text('¿Cómo se supone que se comporta un adulto?', 250, 560);
-  text('¿Cómo se supone que se comporta un adulto?', 220, 570);
-  textSize(55);
+  text('¿Cómo se supone que se comporta un adulto?', 340, 650);
+  text('¿Cómo se supone que se comporta un adulto?', 350, 660);
+  text('¿Cómo se supone que se comporta un adulto?', 310, 670);
+  text('¿Cómo se supone que se comporta un adulto?', 290, 680);
+  text('¿Cómo se supone que se comporta un adulto?', 270, 690);
+  text('¿Cómo se supone que se comporta un adulto?', 250, 700);
+  text('¿Cómo se supone que se comporta un adulto?', 220, 710);
+  textSize(75);
   frameRate(12);
   fill(0, 147, 255);
   text('¿Cómo se supone que se comporta un adulto?', random(125, 480), 390);
   text('¿Cómo se supone que se comporta un adulto?', random(125, 480), 390);
-  textSize(75);
+  textSize(85);
   fill(193, 0, 255);
   text('¿Cómo se supone que se comporta un adulto?', random(155, 440), 300);
-  textSize(68);
+  textSize(78);
   fill(255, 46, 0);
   text('¿Cómo se supone que se comporta un adulto?', random(105, 400), 430);
   textSize(42);
   fill(2, 0, 0);
-  text('¿Cómo se supone que se comporta un adulto?', 90, random(570, 90));
+  text('¿Cómo se supone que se comporta un adulto?', 90, random(970, 90));
   //frameRate(5)
 }
 
